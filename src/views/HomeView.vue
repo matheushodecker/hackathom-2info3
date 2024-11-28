45b2ee75ad531fca056f9deeb529e54bea268d81
matheushodecker <matheus.hodecker.santos@gmail.com>
2024-11-27 21:23:11 -0300

    maudancas:  pagina home re-feita(ainda incompleta)

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,400 +1,132 @@
 <script setup>
-import { RouterView } from 'vue-router'
+import { Swiper, SwiperSlide } from "swiper/vue"; // Importa os componentes do Swiper
+import "swiper/swiper-bundle.css"; // Importa os estilos do Swiper
 </script>
-<template>
-  <div class="background">
-    <main>
-      <div class="inicio">
-        <div class="nada">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum quasi quas porro esse
-          commodi voluptas nihil sapiente accusantium atque, sequi cumque. Ut, tempore quos
-          repellendus iste atque corporis reprehenderit eveniet.
-        </div>
-        <div class="nada">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum quasi quas porro esse
-          commodi voluptas nihil sapiente accusantium atque, sequi cumque. Ut, tempore quos
-          repellendus iste atque corporis reprehenderit eveniet.
-        </div>
-        <div class="nada">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum quasi quas porro esse
-          commodi voluptas nihil sapiente accusantium atque, sequi cumque. Ut, tempore quos
-          repellendus iste atque corporis reprehenderit eveniet.
-        </div>
-        <div class="nada">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum quasi quas porro esse
-          commodi voluptas nihil sapiente accusantium atque, sequi cumque. Ut, tempore quos
-          repellendus iste atque corporis reprehenderit eveniet.
-        </div>
-        <div class="black">
-          <h1>next home</h1>
-          <p class="p_black">Casas de de alto padrão com interiores de luxo</p>
-        </div>
-        <div class="nada">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum quasi quas porro esse
-          commodi voluptas nihil sapiente accusantium atque, sequi cumque. Ut, tempore quos
-          repellendus iste atque corporis reprehenderit eveniet.
-        </div>
-        <div class="nada">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum quasi quas porro esse
-          commodi voluptas nihil sapiente accusantium atque, sequi cumque. Ut, tempore quos
-          repellendus iste atque corporis reprehenderit eveniet.
-        </div>
-        <div class="nada">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum quasi quas porro esse
-          commodi voluptas nihil sapiente accusantium atque, sequi cumque. Ut, tempore quos
-          repellendus iste atque corporis reprehenderit eveniet.
-        </div>
-        <div class="nada">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum quasi quas porro esse
-          commodi voluptas nihil sapiente accusantium atque, sequi cumque. Ut, tempore quos
-          repellendus iste atque corporis reprehenderit eveniet.
-        </div>
-      </div>
-      <div class="container">
-        <div class="item1">
-          <img src="../assets/img/image copy.png" />
-          <div>
-            <h3>Cidade, Pais</h3>
-            <p>tempos em dias</p>
-            <p>preço por noite</p>
-          </div>
-        </div>
 
-        <div class="item2">
-          <img src="../assets/img/image copy 2.png" />
-          <div>
-            <h3>Cidade, Pais</h3>
-            <p>tempos em dias</p>
-            <p>preço por noite</p>
-          </div>
-        </div>
+<template >
+  <section id="img">
 
-        <div class="item3">
-          <img src="../assets/img/image copy 3.png" />
-          <div>
-            <h3>Cidade, Pais</h3>
-            <p>tempos em dias</p>
-            <p>preço por noite</p>
-          </div>
-        </div>
-
-        <div class="item4">
-          <img src="../assets/img/image copy 3.png" />
-          <div>
-            <h3>Cidade, Pais</h3>
-            <p>tempos em dias</p>
-            <p>preço por noite</p>
-          </div>
-        </div>
-
-        <div class="item5">
-          <img src="../assets/img/image copy.png" />
-          <div>
-            <h3>Cidade, Pais</h3>
-            <p>tempos em dias</p>
-            <p>preço por noite</p>
-          </div>
-        </div>
-
-        <div class="item6">
-          <img src="../assets/img/image copy 2.png" />
-          <div>
-            <h3>Cidade, Pais</h3>
-            <p>tempos em dias</p>
-            <p>preço por noite</p>
-          </div>
-        </div>
-
-        <div class="item7">
-          <img src="../assets/img/image copy 3.png" />
-          <div>
-            <h3>Cidade, Pais</h3>
-            <p>tempos em dias</p>
-            <p>preço por noite</p>
-          </div>
-        </div>
-
-        <div class="item8">
-          <img src="../assets/img/image copy 3.png" />
-          <div>
-            <h3>Cidade, Pais</h3>
-            <p>tempos em dias</p>
-            <p>preço por noite</p>
-          </div>
-        </div>
-
-        <div class="item9">
-          <img src="../assets/img/image copy 3.png" />
-          <div>
-            <h3>Cidade, Pais</h3>
-            <p>tempos em dias</p>
-            <p>preço por noite</p>
-          </div>
-        </div>
-
-        <div class="item10">
-          <img src="../assets/img/image copy 3.png" />
-          <div>
-            <h3>Cidade, Pais</h3>
-            <p>tempos em dias</p>
-            <p>preço por noite</p>
-          </div>
-        </div>
-      </div>
-    </main>
-    <footer>
-      <div>
-        <div class="grid">
-          <div class="item">
-            <h3>Clientes</h3>
-            <p class="espaco">
-              Registre-se e receba todas as novidades, promoções e informações sobre os nossos
-              serviços.
-            </p>
-            <ul class="social-media">
-              <li>
-                <a href="#"><i class="fab fa-facebook-f"></i></a>
-              </li>
-              <li>
-                <a href="#"><i class="fab fa-instagram"></i></a>
-              </li>
-              <li>
-                <a href="#"><i class="fab fa-twitter"></i></a>
-              </li>
-              <li>
-                <a href="#"><i class="fab fa-youtube"></i></a>
-              </li>
-            </ul>
-            <p class="espaco">
-              Copyright © 2023 Padaria Grão de Ouro. Todos os direitos reservados.
-            </p>
-          </div>
-          <div class="item">
-            <h3>Recursos</h3>
-            <ul>
-              <li><a href="#">Novidades</a></li>
-              <li><a href="#">Promoções</a></li>
-              <li><a href="#">Serviços</a></li>
-              <li><a href="#">Contato</a></li>
-            </ul>
-          </div>
-          <div class="item">
-            <h3>Fale conosco</h3>
-            <p>Digite o seu endereço de email</p>
-            <input class="email" type="email" placeholder="exemplo@email.com" />
-            <button class="btn">Enviar</button>
-          </div>
-        </div>
-      </div>
-    </footer>
-  </div>
-  <RouterView />
+</section>
+  <H2 class="txt-inicio">Adicionadas recentemente</H2>
+  <Swiper
+    :slides-per-view="5"  
+    :space-between="70" 
+    :loop="false"      
+    longSwipes    
+    grabCursor       
+    pagination       
+         style="border-right: 10px solid transparent; border-left: 15px solid transparent; border-top: 10px solid transparent; border-bottom: 20px solid transparent;"
+  >
+    <!-- Slides -->
+    <SwiperSlide>
+      <div class="column-img">Slide 1</div>
+    </SwiperSlide>
+    <SwiperSlide>
+      <div class="column-img">Slide 2</div>
+    </SwiperSlide>
+    <SwiperSlide>
+      <div class="column-img">Slide 3</div>
+    </SwiperSlide>
+    <SwiperSlide>
+      <div class="column-img">Slide 4</div>
+    </SwiperSlide>
+    <SwiperSlide>
+      <div class="column-img">Slide 5</div>
+    </SwiperSlide>
+    <SwiperSlide>
+      <div class="column-img">Slide 6</div>
+    </SwiperSlide>
+    <SwiperSlide>
+      <div class="column-img">Slide 7</div>
+    </SwiperSlide>
+    <SwiperSlide>
+      <div class="column-img">Slide 8</div>
+    </SwiperSlide>
+    <SwiperSlide>
+      <div class="column-img">Slide 8</div>
+    </SwiperSlide>
+  </Swiper>
+  <H2 class="txt-inicio">Adicionadas recentemente</H2>
+  <Swiper
+  :slides-per-view="5"  
+    :space-between="70" 
+    :loop="false"      
+    longSwipes    
+    grabCursor       
+    pagination       
+         style="border-right: 10px solid transparent; border-left: 15px solid transparent; border-top: 10px solid transparent; border-bottom: 20px solid transparent;"  
+       
+  >
+    <!-- Slides -->
+    <SwiperSlide>
+      <div class="column-img">Slide 1</div>
+    </SwiperSlide>
+    <SwiperSlide>
+      <div class="column-img">Slide 2</div>
+    </SwiperSlide>
+    <SwiperSlide>
+      <div class="column-img">Slide 3</div>
+    </SwiperSlide>
+    <SwiperSlide>
+      <div class="column-img">Slide 4</div>
+    </SwiperSlide>
+    <SwiperSlide>
+      <div class="column-img">Slide 5</div>
+    </SwiperSlide>
+    <SwiperSlide>
+      <div class="column-img">Slide 6</div>
+    </SwiperSlide>
+    <SwiperSlide>
+      <div class="column-img">Slide 7</div>
+    </SwiperSlide>
+    <SwiperSlide>
+      <div class="column-img">Slide 8</div>
+    </SwiperSlide>
+  </Swiper>
 </template>
 
 <style>
-.p_black {
-  color: #f0f0f0;
-  grid-column: 1/3;
-  grid-row: 2;
-  text-align: left;
+.txt-inicio {
+     color: black;
+    margin-top: 50px;
+    margin-left: 135px;
+    font-size: 20pt;
+    font-family: "Libre Bodoni", serif;
 }
-.nada {
-  color: transparent;
-  text-align: left;
-}
-h1 {
-  color: #f0f0f0;
-  grid-column: 1/3;
-  grid-row: 1;
-  text-align: left; 
-}
-.black {
-  display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  grid-template-rows: repeat(3, 0.25fr);
-  background-color: rgb(24, 24, 24);
-  border: 100px solid rgb(24, 24, 24);
-  opacity: 0.7;
-  grid-row: 2;
-  grid-column: 2;
-}
-.inicio {
-  display: grid;
-  grid-template-columns: 1fr 5fr 1fr;
-  grid-template-rows: 0.1fr 5fr 3fr;
-  background-image: url(../assets/img/image.png);
-  background-size: cover;
-  position: relative;
-  width: 100%;
-  height: 600px;
-}
-.container {
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  grid-template-rows: repeat(4, 1fr);
-  row-gap: 20px;
-  column-gap: 100px;
-
-  padding-left: 100px;
-  padding-top: 100px;
-  padding-bottom: 100px;
-  padding-right: 100px;
-}
-
-.navbar {
-  background-color: #676767;
-  height: 80px;
+/* Adicione um pouco de estilo aos slides */
+.column-img {
+  height: 300px;
+  width: 300px;
+  background-color: lightgray;
   display: flex;
   align-items: center;
+  justify-content: center;
+  font-size: 20px;
+  border-radius: 10px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+}
+.swiper-button-next,
+.swiper-button-prev {
+ display: none;
 }
 
-.item1,
-.item2 {
-  background-color: #f0f0f0;
-  padding: 20px;
-
-  grid-row: 1;
-  min-height: 100px;
-}
-
-.item3,
-.item4 {
-  background-color: #f0f0f0;
-  padding: 20px;
-  grid-row: 2;
-  min-height: 100px;
-}
-
-.item1,
-.item2,
-.item3,
-.item4 {
-  display: grid;
-  grid-template-rows: repeat(1, 1fr);
-  border-radius: 30px;
-  text-align: left;
-  background-color: transparent;
-}
-.item5,
-.item6 {
-  background-color: #f0f0f0;
-  padding: 20px;
-
-  grid-row: 3;
-  min-height: 100px;
-}
-
-.item7,
-.item8 {
-  background-color: #f0f0f0;
-  padding: 20px;
-  grid-row: 4;
-  min-height: 100px;
-}
-
-.item9,
-.item10{
-  background-color: #f0f0f0;
-  padding: 20px;
-  grid-row: 5;
-  min-height: 100px;
-}
-
-.item5,
-.item6,
-.item7,
-.item8,
-.item9,
-.item10 {
-  display: grid;
-  grid-template-rows: repeat(1, 1fr);
-  border-radius: 30px;
-  text-align: left;
-  background-color: transparent;
-}
-img {
-  width: 100%;
-  height: 92%;
-  align-items: center;
-  transition: transform 0.5s cubic-bezier(0.25, 0.1, 0.25, 1);
-  transform-origin: center;
-}
-img:hover {
-  transform: scale(1.1);
-}
-footer {
-  background-color: #676767;
-  color: #fff;
-  padding: 40px 0;
-}
-
-footer .grid {
-  display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
-  gap: 20px;
-}
-
-footer h3 {
-  margin-top: 0;
-  margin-bottom: 20px;
-}
-
-footer ul {
-  list-style: none;
-  margin: 0;
-  padding: 0;
-}
-
-footer li {
-  margin-bottom: 10px;
-}
-
-footer a {
-  text-decoration: none;
-  color: #fff;
-}
-
-.social-media li {
-  display: inline-block;
-  margin-right: 10px;
-}
-
-.social-media a {
-  font-size: 1.5em;
-  color: #fff;
-}
-
-.social-media a:hover {
-  line-height: 10px;
-  color: #ffc107;
-}
-
-input[type='email'] {
-  width: 100%;
-  margin-right: 10px;
-  border: 1px solid #ddd;
-  border-radius: 5px;
-  border-color: #000000;
-  margin-bottom: 10px;
-}
-
-.item {
-  text-align: center;
-  padding: 20px;
-}
-.espaco {
-  line-height: 20px;
-}
-.textR{
-  color: #000000;
-}
-
-.backgrund {
-  background-image: url(../assets/img/image.png);
+#img {
+  background-image: url(/src/assets/img/casa-de-luxo-7-1.jpg);
   background-size: cover;
-  position: relative;
-  width: 100%;
-  height: 600px;
+  box-shadow: rgba(50, 50, 93, 0.25) 0px 30px 60px -12px inset, rgba(0, 0, 0, 0.3) 0px 18px 36px -18px inset;
+  height: 90vh;
 }
-
+.column {
+  display: flex;
+  gap: 40px;
+  justify-content: center;
+  margin-top: 20px;
+}
+.column-img{
+  height: 300px;
+  width: 300px;
+  margin-bottom: 50px;
+  border-radius: 20px;;
+  box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+}
 </style>
